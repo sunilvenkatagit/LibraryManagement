5c10ca8a0d9b8ff1cc7bb902cb57514891df83ac
Venkata Sunil Puchaginjala <sunil.puchaginjala@cgi.com>
2021-10-17 17:06:57 -0700

    Registered Automapper & MediatR

---
+++ LibraryManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace LibraryManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServives(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ LibraryManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LibraryManagement.Application.Features.Libraries.Queries.GetLibrariesList;
+using LibraryManagement.Domain.Entities;
+
+namespace LibraryManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Library, LibraryListVm>().ReverseMap();
+        }
+    }
+}
