5024ba720fc5134a0a670d8fff75dad781af062c
Venkata Sunil Puchaginjala <sunil.puchaginjala@cgi.com>
2021-10-23 10:20:33 -0700

    Add PeristenceServices

---
+++ LibraryManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using LibraryManagement.Application.Contracts.Persistence;
+using LibraryManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPeristenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LibraryManagementDbContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("LibraryManagementConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+
+            return services;
+        }
+    }
+}
