252431518bfc1daa13dbe2f7b7a8112e3f373ecd
Venkata Sunil Puchaginjala <sunil.puchaginjala@cgi.com>
2021-11-02 09:07:23 -0700

    Add logging support using Serilog

--- LibraryManagement.Api/Program.cs
+++ LibraryManagement.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,27 @@ namespace LibraryManagement.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            Log.Logger = new LoggerConfiguration()
+                            .ReadFrom.Configuration(config)
+                            .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+                            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting the application");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An error occured while starting the application");
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- LibraryManagement.Application/Features/Libraries/Queries/GetLibrariesList/GetLibrariesListQueryHandler.cs
+++ LibraryManagement.Application/Features/Libraries/Queries/GetLibrariesList/GetLibrariesListQueryHandler.cs
@@ -2,6 +2,7 @@
 using LibraryManagement.Application.Contracts.Persistence;
 using LibraryManagement.Domain.Entities;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -13,15 +14,18 @@ namespace LibraryManagement.Application.Features.Libraries.Queries.GetLibrariesL
     {
         private readonly IAsyncRepository<Library> _libraryRepository;
         private readonly IMapper _mapper;
+        private readonly ILogger<GetLibrariesListQueryHandler> _logger;
 
-        public GetLibrariesListQueryHandler(IMapper mapper, IAsyncRepository<Library> libraryRepository)
+        public GetLibrariesListQueryHandler(IMapper mapper, IAsyncRepository<Library> libraryRepository, ILogger<GetLibrariesListQueryHandler> logger)
         {
             _mapper = mapper;
             _libraryRepository = libraryRepository;
+            _logger = logger;
         }
 
         public async Task<List<LibraryListVm>> Handle(GetLibrariesListQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Getting all libraries");
             var allLibraries = (await _libraryRepository.LisAllAsync()).OrderBy(l => l.Name);
             return _mapper.Map<List<LibraryListVm>>(allLibraries);
         }
